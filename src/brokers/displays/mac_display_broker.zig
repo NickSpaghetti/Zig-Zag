a121e602b7dd063f2d17cefb4191000741e116fc
NickSpaghetti <nicholas_cicchetti@glic.com>
2024-11-19 13:44:27 -0500

    added a fn to get all displays on mac

--- src/brokers/displays/mac_display_broker.zig
+++ src/brokers/displays/mac_display_broker.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const c = @cImport({
     @cInclude("CoreGraphics/CoreGraphics.h");
     @cInclude("CoreFoundation/CoreFoundation.h");
@@ -14,5 +15,16 @@ pub const MacDisplayBroker = struct {
 
     pub fn getDisplayID() u32 {
         return c.CGMainDisplayID();
+    }
+
+    pub fn getDisplayIds() []u32 {
+        const maxDisplays: c.unit32_t = 100;
+        var displays: [maxDisplays]c.CGDirectDisplayID = undefined;
+        var displayCount: c.unit32_t = 0;
+        const result = c.CGGetActiveDisplayList(maxDisplays, &displays, &displayCount);
+        if (result != c.kCGErrorSuccess) {
+            std.debug.panic("failed to get list of active displays.", .{});
+        }
+        return displays[0..displayCount];
     }
 };
